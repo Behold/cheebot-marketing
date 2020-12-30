886032ffdf889126bb008f75900ffb60030121b6
Joe Larama <joelarama@gmail.com>
2020-12-30 10:00:47 -0800

    more updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,33 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          light: '#FF893A',
-          lighter: '#ffa366',
-          lightest: '#feecdc',
-          DEFAULT: '#FF6600',
-          dark: '#F46200',
+          DEFAULT: '#0AC1C1',
+          light: '#85EAEA',
+          lighter: '#c5ecec',
+          lightest: '#e5f6f6',
+          dark: '#00A3A3'
+        },
+        secondary: {
+          DEFAULT: '#0D5BBC',
+          lighter: '#E2EFFF',
+          lightest: '#F6F9FD',
+          dark: '#0A2C5F',
+          darker: '#000D33'
         },
         gray: {
           100: '#F7F7F7',
           dark: '#333333'
         }
+      },
+      boxShadow: {
+        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.025)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.05), 0 2px 4px -1px rgba(0, 0, 0, 0.025)',
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        none: 'none',
       },
       fontFamily: {
         sans: [
@@ -44,6 +61,11 @@ module.exports = {
         '7xl': ['4.5rem', { lineHeight: '1' }],
         '8xl': ['6rem', { lineHeight: '1' }],
         '9xl': ['8rem', { lineHeight: '1' }],
+      },
+      height: {
+        'screen-1/2': '50vh',
+        'screen-2/3': '66.666667vh',
+        'screen-3/4': '75vh',
       },
     },
   },
